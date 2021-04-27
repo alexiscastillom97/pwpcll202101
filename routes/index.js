6b017e7ea3432cb02a1053bad0e7cb67998c6c35
alexiscastillo97 <alexis.castillo_1g@hotmail.com>
2021-04-27 17:07:26 -0500

    Agregando nombre de compañia

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express', author: 'Alexis Castillo M', appName: 'WebApp' });
+    res.render('index', { title: 'Express', author: 'Alexis Castillo M', appName: 'WebApp', company: 'Awsom Software' });
 });
 //Agragando nueva ruta
 router.get('/greeting', function(req, res, next) {

ff9e8801b662eb5a067465d750273d36b067e787
alexiscastillo97 <alexis.castillo_1g@hotmail.com>
2021-05-28 21:55:45 -0500

    Sesion 27

--- server/app.js
+++ server/app.js
@@ -4,10 +4,9 @@
 //var cookieParser = require('cookie-parser');
 //var logger = require('morgan');
 
-import createError from 'http-errors'
+import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
-
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
@@ -17,38 +16,41 @@ import usersRouter from '@s-routes/users';
 //var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 
-
-//Webpack Modules 
+//Webpack Modules
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../webpack.dev.config';
 import webpackDevConfig from '../webpack.dev.config';
 
-//Consultar el modo en que se esta ejecutando la aplicacion 
+//Consultar el modo en que se esta ejecutando la aplicacion
 const env = process.env.SETNODE_ENV || 'developement';
 
-//Se crea la aplicacion express 
-var app = express();
+//Se crea la aplicacion express
+const app = express();
 
-//Verificando el modo de ejecucion de la aplicacion 
+//Verificando el modo de ejecucion de la aplicacion
 if (env === 'development') {
     console.log('> Executing in Development Mode: Webpack Hot Reloading');
     //Paso 1: Agregando la ruta HMR
-    // reload=true: Habilita la recarga del frontend cuando hay cambios en el codigo fuente del frontend 
-    //timeout=1000: Tiempo de espera entre recarga y recarga de la pagina 
-    webpackConfig.entry = ['webpack-hot-middleware/client?reload=true&timeout=1000', webpackConfig.entry];
+    // reload=true: Habilita la recarga del frontend cuando hay cambios en el codigo fuente del frontend
+    //timeout=1000: Tiempo de espera entre recarga y recarga de la pagina
+    webpackDevConfig.entry = [
+        'webpack-hot-middleware/client?reload=true&timeout=1000',
+        webpackDevConfig.entry,
+    ];
 
-    //Paso 2: Agregamos el plugin 
-    webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+    //Paso 2: Agregamos el plugin
+    webpackDevConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
 
-    //Paso 3: Crear el compilador de webpack 
-    const compiler = webpack(webpackConfig);
+    //Paso 3: Crear el compilador de webpack
+    const compiler = webpack(webpackDevConfig);
 
-    //Paso 4: Agregando el middleware a la cadena de midedlewares de nuestra aplicacion 
-    app.use(webpackDevMiddleware(compiler, {
-        publicPath: webpackDevConfig.output.publicPath
-    }));
+    //Paso 4: Agregando el middleware a la cadena de midedlewares de nuestra aplicacion
+    app.use(
+        webpackDevMiddleware(compiler, {
+            publicPath: webpackDevConfig.output.publicPath,
+        }),
+    );
 
     //Paso 5: Agregando el Webpack Hot Middleware
     app.use(webpackHotMiddleware(compiler));
@@ -70,12 +72,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};

f7f3c1b5cade4bf6212abab2a1e2fc8d8ccd6b25
alexiscastillo97 <alexis.castillo_1g@hotmail.com>
2021-06-29 21:43:24 -0500

    Sesion 34

--- server/routes/home.js
+++ server/routes/home.js
@@ -8,10 +8,14 @@ import homeController from '@server/controllers/homeController';
 const router = new Router();
 
 // GET '/'
-router.get('/', homeController.index);
+router.get(['/', '/index'], homeController.index);
+
 
 // GET '/greeting'
 router.get('/greeting', homeController.greeting)
+
+// GET '//about' 
+router.get('/about', homeController.about);
 
 // Exportando el router que maneja las subrutas 
 //para el controlador Home
